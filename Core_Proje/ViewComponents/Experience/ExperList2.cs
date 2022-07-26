755303202ab61ee5cb3ef517a5844da56e10ab77
gokhanyuce92 <g.yuce92@gmail.com>
2022-07-26 22:28:18 +0300

    Add project files.

---
+++ Core_Proje/ViewComponents/Experience/ExperList2.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.ViewComponents.Experience
+{
+    public class ExperList2 : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
